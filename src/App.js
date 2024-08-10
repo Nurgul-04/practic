df65423664f7e6bd8849732a8c30e5ba6281c605
Nurgul-04 <sabirkyzynurgul@gmail.com>
2024-08-10 14:29:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Compon from './component/Compon';
 
-function App() {
+function App(props) {
+  const [test, setTest]=useState(false)
+  const a = ()=>{
+    setTest(true)
+  }
+  const b = ()=>{
+    setTest(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={a}> Click</button>
+     {test===true ? <Compon onClose={b}/>:null}
     </div>
   );
 }
---
+++ src/component/Compon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Compon = (props) => {
+  
+    
+  return (
+    <div>       
+     <h1>Мен,Маматсабир кызы Нургул.</h1>
+     <button onClick={props.onClose}>Close</button>
+    
+    </div>
+  )
+}
+
+export default Compon
